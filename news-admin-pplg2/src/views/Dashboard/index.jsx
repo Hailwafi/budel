8a652fd86f2b1bc8e25bbceaa9643bc4d2317a57
Hailwafi <hailwafipura.a@gmail.com>
2024-01-19 11:20:04 +0700

    budel

---
+++ news-admin-pplg2/src/views/Dashboard/index.jsx
@@ -0,0 +1,10 @@
+//import layout
+import LayoutDefault from "../../layouts/Default.jsx";
+
+export default function Dashboard () {
+    return (
+        <LayoutDefault>
+            <h1 style={{color: 'white'}}>Halaman Dashboard</h1>
+        </LayoutDefault>
+    )
+}
